c0f7154068f8f77f86b0e410b43516c3e570a389
Aicha Benjdir <aicha.benjdir@isimg.tn>
2025-03-12 12:09:37 +0100

    ajout des messages d'erreur et de succès pour le formulaire de connexion

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,23 +10,33 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent {
   email: string = '';
   password: string = '';
-  
+  errorMessage: string = '';  // Message d'erreur
+  successMessage: string = ''; // Message de succès
+
   constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit(): void {
+    this.errorMessage = ''; // Réinitialisation des messages
+    this.successMessage = '';
+
     this.authService.login(this.email, this.password).subscribe((response) => {
       if (response.isAuthenticated) {
         console.log('Connexion réussie');
-        // Sauvegarder l'utilisateur dans le localStorage pour l'utiliser ailleurs
-        localStorage.setItem('currentUser', JSON.stringify(response.user));  // Stocke l'utilisateur sous la clé 'currentUser'
+        localStorage.setItem('currentUser', JSON.stringify(response.user));
         
-        // Redirection vers le tableau de bord
-        this.router.navigate(['/dashboard']);
+        // Affichage du message de succès
+        this.successMessage = 'Connexion réussie, redirection en cours...';
+
+        setTimeout(() => {
+          this.router.navigate(['/dashboard']);
+        }, 2000); // Rediriger après 2 secondes
       } else {
         console.error('Identifiants invalides');
-        // Afficher un message d'erreur, si nécessaire
+        this.errorMessage = 'Email ou mot de passe incorrect';
       }
+    }, (error) => {
+      console.error('Erreur de connexion', error);
+      this.errorMessage = 'Une erreur est survenue. Veuillez réessayer.';
     });
   }
-  
 }
